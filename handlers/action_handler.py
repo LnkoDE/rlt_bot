fb6ad6a1953635a59b0b2da8c187deb34a0c4b01
Lyashenko Denis <lyashenko.lanceru@gmail.com>
2024-05-28 16:50:47 +0300

    added exceptions

--- handlers/action_handler.py
+++ handlers/action_handler.py
@@ -6,10 +6,21 @@ from texts.maintext import any_answer
 action_handler_router = Router()
 
 @action_handler_router.message()
-async def start_cmd(message: types.Message):
-    data = eval(message.text)
-    dt_from = data["dt_from"]
-    dt_upto = data["dt_upto"]
-    group_type = data["group_type"]
-    answer = main_action.aggregatedb_bygroup(dt_from, dt_upto, group_type)
-    await message.answer(f'{answer}')
+async def data_action(message: types.Message):
+    try:
+        data = eval(message.text)
+        dt_from = data["dt_from"]
+        dt_upto = data["dt_upto"]
+        group_type = data["group_type"]
+        answer = main_action.aggregatedb_bygroup(dt_from, dt_upto, group_type)
+        await message.answer(f'{answer}')
+    except KeyError:
+        await message.answer(any_answer)
+    except ValueError:
+        await message.answer(any_answer)
+    except TypeError:
+        await message.answer(any_answer)
+    except IndentationError:
+        await message.answer(any_answer)
+    except SyntaxError:
+        await message.answer(any_answer)
