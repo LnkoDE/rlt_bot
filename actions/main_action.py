8e850e3b76567c9734bad138407cbd0859b60152
Lyashenko Denis <lyashenko.lanceru@gmail.com>
2024-05-28 15:34:23 +0300

    main action added date format

--- actions/main_action.py
+++ actions/main_action.py
@@ -1,4 +1,5 @@
-import os
+import os, datetime
+
 from pymongo import MongoClient
 
 from dotenv import find_dotenv, load_dotenv
@@ -12,3 +13,12 @@ client = MongoClient(HOST, PORT)
 db = client.test
 coll = db.sample_collection
 
+# Пример запроса 
+dt_from = "2022-09-01T00:00:00"
+dt_upto = "2022-12-31T23:59:00"
+group_type = "month"
+
+# Переформатирование даты из запроса
+dt_format = "%Y-%m-%dT%H:%M:%S"
+query_dt_from = datetime.datetime.strptime(dt_from, dt_format)
+query_dt_upto = datetime.datetime.strptime(dt_upto, dt_format)
